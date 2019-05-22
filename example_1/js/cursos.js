a75217f7a869e7639698ef6c14d8135579e3a421
iam3mer <baorjs@gmail.com>
2019-05-22 01:17:47 -0500

    cursos ofertados

---
+++ example_1/js/cursos.js
@@ -0,0 +1,27 @@
+let curso = [{
+        id: 1,
+        curso: 'Node.JS',
+        horas: 70,
+        valor: 50
+    },
+    {
+        id: 2,
+        curso: 'HTML5',
+        horas: 25,
+        valor: 15,
+    },
+    {
+        id: 3,
+        curso: 'CSS3',
+        horas: 60,
+        valor: 40
+    },
+    
+        id: 4,
+        curso: 'Diseño Grafico',
+        horas: 30,
+        valor: 50
+    }
+];
+
+module.exports = { curso }
